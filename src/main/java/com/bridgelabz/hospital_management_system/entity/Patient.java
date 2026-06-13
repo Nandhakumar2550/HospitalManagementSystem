fc43ff64aad32e030adb7dc81a5e52daa2e1cfeb
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-06-13 14:52:49 +0530

    [Nandhakumar] UC2 - Create Entity Classes

---
+++ src/main/java/com/bridgelabz/hospital_management_system/entity/Patient.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.hospital_management_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String gender;
+
+    private String disease;
+
+    private String address;
+
+    private String phoneNumber;
+}
---
+++ src/main/java/com/bridgelabz/hospital_management_system/entity/Role.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.hospital_management_system.entity;
+
+public enum Role {
+    ADMIN
+}
