e3e81d33dbd5782004883b8c901440c4a53ec83b
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-06-13 14:56:46 +0530

    [Nandhakumar] UC3 - Create Repository Layer

---
+++ src/main/java/com/bridgelabz/hospital_management_system/repository/PatientRepository.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hospital_management_system.repository;
+
+import com.bridgelabz.hospital_management_system.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
